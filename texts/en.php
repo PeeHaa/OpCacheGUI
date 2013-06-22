95b16247ff7f2729e0e08e1c420f583abd903bd6
PeeHaa <info@pieterhordijk.com>
2013-06-22 23:14:37 +0200

    Added English status translations

---
+++ texts/en.php
@@ -0,0 +1,9 @@
+<?php
+
+$texts = [
+    'status.title'                  => 'Status',
+    'status.opcache_enabled'        => 'Enabled',
+    'status.cache_full'             => 'Cache full',
+    'status.restart_pending'        => 'Restart pending',
+    'status.restart_in_progress'    => 'Restart in progress',
+];
