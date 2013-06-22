9c7ceb4187a038d6ed2ff50a0c09250fcd917a5a
PeeHaa <info@pieterhordijk.com>
2013-06-22 22:09:18 +0200

    Added library bootstrapper

---
+++ src/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bootstrap the library
+ *
+ * PHP version 5.5
+ *
+ * @category   OpCacheGUI
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace OpCacheGUI;
+
+use OpCacheGUI\Core\Autoloader;
+
+require_once __DIR__ . '/Core/Autoloader.php';
+
+$autoloader = new Autoloader(__NAMESPACE__, dirname(__DIR__));
+$autoloader->register();
