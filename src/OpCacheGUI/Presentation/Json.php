1db27df7f15fab0955dffb8e7e44debe7c4b403c
PeeHaa <info@pieterhordijk.com>
2014-10-06 18:43:18 +0200

    Fixed version number

--- src/OpCacheGUI/Presentation/Json.php
+++ src/OpCacheGUI/Presentation/Json.php
@@ -9,7 +9,7 @@
  * @author     Pieter Hordijk <info@pieterhordijk.com>
  * @copyright  Copyright (c) 2014 Pieter Hordijk <https://github.com/PeeHaa>
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version    2.0.0
+ * @version    1.0.0
  */
 namespace OpCacheGUI\Presentation;
 
