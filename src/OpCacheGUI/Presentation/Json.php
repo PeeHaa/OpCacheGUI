3d49d92c20cca0c4b4e4d48c682d61180af52b62
PeeHaa <info@pieterhordijk.com>
2014-10-06 18:35:25 +0200

    Added JSON template system

---
+++ src/OpCacheGUI/Presentation/Json.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The class is responsible for rendering JSON templates
+ *
+ * PHP version 5.5
+ *
+ * @category   OpCacheGUI
+ * @package    Presentation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2014 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    2.0.0
+ */
+namespace OpCacheGUI\Presentation;
+
+use OpCacheGUI\I18n\Translator;
+
+/**
+ * The class is responsible for rendering JSON templates
+ *
+ * @category   OpCacheGUI
+ * @package    Presentation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Json extends Template
+{
+    /**
+     * Renders a template
+     *
+     * @param string $template The template to render
+     * @param array  $data     The data to use in the template
+     */
+    public function render($template, array $data = [])
+    {
+        $this->variables = $data;
+
+        return require $this->templateDirectory . '/' . $template;
+    }
+}
