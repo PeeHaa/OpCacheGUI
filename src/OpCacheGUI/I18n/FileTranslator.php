ab1e6039ae14e51b3b00c79eaf71a3dccaae6beb
PeeHaa <info@pieterhordijk.com>
2014-10-08 01:55:08 +0200

    Added translator tests

--- src/OpCacheGUI/I18n/FileTranslator.php
+++ src/OpCacheGUI/I18n/FileTranslator.php
@@ -35,9 +35,9 @@ class FileTranslator implements Translator
      * @throws \Exception When the language is not supported (i.e. no translation file can be found for the language)
      * @throws \Exception When the translation file is invalid (i.e. no `$texts` array present)
      */
-    public function __construct($languageCode)
+    public function __construct($translationDirectory, $languageCode)
     {
-        $translationFile = __DIR__ . '/../../../texts/' . $languageCode . '.php';
+        $translationFile = $translationDirectory . '/' . $languageCode . '.php';
 
         if (!file_exists($translationFile)) {
             throw new \Exception('Unsupported language (`' . $languageCode . '`).');
