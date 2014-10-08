6000cecc21a91f3a90a25163ea8627e81ae9ca5b
PeeHaa <info@pieterhordijk.com>
2014-10-08 03:18:05 +0200

    Added classcycler test

---
+++ test/Unit/Presentation/ClassCyclerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace OpCacheGUITest\Presentation;
+
+use OpCacheGUI\Presentation\ClassCycler;
+
+class ClassCyclerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers OpCacheGUI\Presentation\ClassCycler::__construct
+     * @covers OpCacheGUI\Presentation\ClassCycler::next
+     */
+    public function testNextFirst()
+    {
+        $cycler = new ClassCycler([1, 2]);
+
+        $this->assertSame(1, $cycler->next());
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\ClassCycler::__construct
+     * @covers OpCacheGUI\Presentation\ClassCycler::next
+     */
+    public function testNextMultiple()
+    {
+        $cycler = new ClassCycler([1, 2]);
+
+        $this->assertSame(1, $cycler->next());
+        $this->assertSame(2, $cycler->next());
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\ClassCycler::__construct
+     * @covers OpCacheGUI\Presentation\ClassCycler::next
+     * @covers OpCacheGUI\Presentation\ClassCycler::rewind
+     */
+    public function testNextOverflows()
+    {
+        $cycler = new ClassCycler([1, 2]);
+
+        $this->assertSame(1, $cycler->next());
+        $this->assertSame(2, $cycler->next());
+        $this->assertSame(1, $cycler->next());
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\ClassCycler::__construct
+     * @covers OpCacheGUI\Presentation\ClassCycler::next
+     * @covers OpCacheGUI\Presentation\ClassCycler::rewind
+     */
+    public function testRewind()
+    {
+        $cycler = new ClassCycler([1, 2]);
+
+        $this->assertSame(1, $cycler->next());
+        $cycler->rewind();
+        $this->assertSame(1, $cycler->next());
+    }
+}
