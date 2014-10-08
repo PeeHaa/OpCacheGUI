b472d2c8369bd5bc76b20e0dfd538401a3f7dbef
PeeHaa <info@pieterhordijk.com>
2014-10-08 04:40:57 +0200

    Added json template tests

---
+++ test/Unit/Presentation/JsonTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OpCacheGUITest\Presentation;
+
+use OpCacheGUI\Presentation\Json;
+
+class JsonTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers OpCacheGUI\Presentation\Json::__construct
+     */
+    public function testConstructCorrectInterface()
+    {
+        $json = new Json(__DIR__, $this->getMock('\\OpCacheGui\\I18n\\Translator'));
+
+        $this->assertInstanceOf('\\OpCacheGUI\\Presentation\\Renderer', $json);
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\Json::__construct
+     */
+    public function testConstructCorrectInstance()
+    {
+        $json = new Json(__DIR__, $this->getMock('\\OpCacheGui\\I18n\\Translator'));
+
+        $this->assertInstanceOf('\\OpCacheGUI\\Presentation\\Template', $json);
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\Json::__construct
+     * @covers OpCacheGUI\Presentation\Json::render
+     */
+    public function testRender()
+    {
+        $json = new Json(__DIR__ . '/../../Data/templates', $this->getMock('\\OpCacheGui\\I18n\\Translator'));
+
+        $this->assertSame('content', $json->render('example.pjson'));
+    }
+}
