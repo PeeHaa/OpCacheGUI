16727d6be763112e21c798f8580d31b6bad201e5
PeeHaa <info@pieterhordijk.com>
2014-10-08 02:13:14 +0200

    Added routefactory tests

---
+++ test/Unit/Network/RouteFactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OpCacheGUITest\Network;
+
+use OpCacheGUI\Network\RouteFactory;
+
+class RouteFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     */
+    public function testConstructCorrectInstance()
+    {
+        $factory = new RouteFactory;
+
+        $this->assertInstanceOf('\\OpCacheGUI\\Network\\RouteBuilder', $factory);
+    }
+
+    /**
+     * @covers OpCacheGUI\Network\RouteFactory::build
+     */
+    public function testBuild()
+    {
+        $factory = new RouteFactory;
+
+        $this->assertInstanceOf('\\OpCacheGUI\\Network\\Route', $factory->build('id', 'GET', function(){}));
+    }
+}
