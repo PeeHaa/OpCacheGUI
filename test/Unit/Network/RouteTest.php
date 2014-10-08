0c681eeedcbfae41bd7afd3016e3b6ed5a112491
PeeHaa <info@pieterhordijk.com>
2014-10-08 02:09:28 +0200

    Added route tests

---
+++ test/Unit/Network/RouteTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace OpCacheGUITest\Network;
+
+use OpCacheGUI\Network\Route;
+
+class RouteTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers OpCacheGUI\Network\Route::__construct
+     * @covers OpCacheGUI\Network\Route::matchesRequest
+     */
+    public function testMatchesRequestNoMatch()
+    {
+        $route = new Route('id', 'GET', function(){});
+
+        $this->assertFalse($route->matchesRequest('notid', 'notGET'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Network\Route::__construct
+     * @covers OpCacheGUI\Network\Route::matchesRequest
+     */
+    public function testMatchesRequestNoMatchIdentifier()
+    {
+        $route = new Route('id', 'GET', function(){});
+
+        $this->assertFalse($route->matchesRequest('notid', 'GET'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Network\Route::__construct
+     * @covers OpCacheGUI\Network\Route::matchesRequest
+     */
+    public function testMatchesRequestNoMatchVerb()
+    {
+        $route = new Route('id', 'GET', function(){});
+
+        $this->assertFalse($route->matchesRequest('id', 'notGET'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Network\Route::__construct
+     * @covers OpCacheGUI\Network\Route::matchesRequest
+     */
+    public function testMatchesRequestValid()
+    {
+        $route = new Route('id', 'GET', function(){});
+
+        $this->assertTrue($route->matchesRequest('id', 'GET'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Network\Route::__construct
+     * @covers OpCacheGUI\Network\Route::run
+     */
+    public function testRun()
+    {
+        $route = new Route('id', 'GET', function(){
+            return 'foo';
+        });
+
+        $this->assertSame($route->run(), 'foo');
+    }
+}
