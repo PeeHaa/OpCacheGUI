f1018c359440b742a01a106df2a75edbb0673cf5
PeeHaa <info@pieterhordijk.com>
2014-10-08 16:54:51 +0200

    Added Url renderer tests

---
+++ test/Unit/Presentation/UrlTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace OpCacheGUITest\Presentation;
+
+use OpCacheGUI\Presentation\Url;
+use OpCacheGUI\Network\Router;
+
+class UrlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers OpCacheGUI\Presentation\Url::__construct
+     */
+    public function testConstructCorrectInterface()
+    {
+        $url = new Url(Router::URL_REWRITE);
+
+        $this->assertInstanceOf('\\OpCacheGUI\\Presentation\\UrlRenderer', $url);
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\Url::__construct
+     * @covers OpCacheGUI\Presentation\Url::get
+     * @covers OpCacheGUI\Presentation\Url::getRewriteUrl
+     */
+    public function testGetRewriteUrlStatus()
+    {
+        $url = new Url(Router::URL_REWRITE);
+
+        $this->assertSame('..', $url->get('status'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\Url::__construct
+     * @covers OpCacheGUI\Presentation\Url::get
+     * @covers OpCacheGUI\Presentation\Url::getRewriteUrl
+     */
+    public function testGetRewriteUrlOther()
+    {
+        $url = new Url(Router::URL_REWRITE);
+
+        $this->assertSame('/other', $url->get('other'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\Url::__construct
+     * @covers OpCacheGUI\Presentation\Url::get
+     * @covers OpCacheGUI\Presentation\Url::getQueryStringUrl
+     */
+    public function testGetQueryStringUrlStatus()
+    {
+        $url = new Url(Router::QUERY_STRING);
+
+        $this->assertSame('?', $url->get('status'));
+    }
+
+    /**
+     * @covers OpCacheGUI\Presentation\Url::__construct
+     * @covers OpCacheGUI\Presentation\Url::get
+     * @covers OpCacheGUI\Presentation\Url::getQueryStringUrl
+     */
+    public function testGetQueryStringUrlOther()
+    {
+        $url = new Url(Router::QUERY_STRING);
+
+        $this->assertSame('?other', $url->get('other'));
+    }
+}
