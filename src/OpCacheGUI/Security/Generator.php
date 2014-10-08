2471d2b09649e8bf23c2d5882f10a099f0ea6833
PeeHaa <info@pieterhordijk.com>
2014-10-08 19:37:14 +0200

    Added generators

---
+++ src/OpCacheGUI/Security/Generator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Generates a random string using mcrypt
+ *
+ * PHP version 5.5
+ *
+ * @category   OpCacheGUI
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2014 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace OpCacheGUI\Security;
+
+/**
+ * Generates a random string using mcrypt
+ *
+ * @category   OpCacheGUI
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Generator
+{
+    /**
+     * Generates a random string
+     *
+     * @param int $length The length of the random string to be generated
+     */
+    public function generate($length);
+}
