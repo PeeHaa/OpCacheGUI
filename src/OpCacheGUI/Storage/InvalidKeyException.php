4edf81114fabb0b08bbc030b44f8be78e45667e5
PeeHaa <info@pieterhordijk.com>
2014-10-08 17:44:03 +0200

    Added storage exception

---
+++ src/OpCacheGUI/Storage/InvalidKeyException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Storage exception class for when trying to access an invalid store
+ *
+ * PHP version 5.5
+ *
+ * @category   OpCacheGUI
+ * @package    Storage
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2014 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace OpCacheGUI\Storage;
+
+/**
+ * Storage exception class for when trying to access an invalid store
+ *
+ * @category   OpCacheGUI
+ * @package    Storage
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidKeyException extends \Exception
+{
+}
