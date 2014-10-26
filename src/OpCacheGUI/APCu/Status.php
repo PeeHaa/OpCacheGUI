65345649548337701ebaffce2aa42df6c2391267
PeeHaa <info@pieterhordijk.com>
2014-10-26 19:48:07 +0100

    Added get memory info method

--- src/OpCacheGUI/APCu/Status.php
+++ src/OpCacheGUI/APCu/Status.php
@@ -47,17 +47,24 @@ class Status
     private $cacheStatus;
 
     /**
+     * @var array The (unfiltered) output of apcu_sma_info()
+     */
+    private $memoryStatus;
+
+    /**
      * Creates instance
      *
      * @param \OpCacheGUI\Format\Byte        $byteFormatter Formatter of byte values
      * @param \OpCacheGUI\APCu\Configuration $config        The config of APCu
      * @param array                          $cacheStatus   The (unfiltered) output of apcu_cache_info()
+     * @param array                          $memoryStatus  The (unfiltered) output of apcu_sma_info()
      */
-    public function __construct(Byte $byteFormatter, Configuration $config, array $cacheStatus)
+    public function __construct(Byte $byteFormatter, Configuration $config, array $cacheStatus, array $memoryStatus)
     {
         $this->byteFormatter = $byteFormatter;
         $this->config        = $config;
         $this->cacheStatus   = $cacheStatus;
+        $this->memoryStatus  = $memoryStatus;
     }
 
     /**
@@ -111,6 +118,93 @@ class Status
         $timeAgo .= $diff->i > 1 ? 's' : '';
 
         return $timeAgo;
+    }
+
+    /**
+     * Gets the memory info of APCu
+     *
+     * @return array The memory info
+     */
+    public function getMemoryInfo()
+    {
+        $size = $this->memoryStatus['num_seg'] * $this->memoryStatus['seg_size'];
+
+        return [
+            'total_memory'            => $this->byteFormatter->format($size),
+            'used_memory'             => $this->byteFormatter->format($size - $this->memoryStatus['avail_mem']),
+            'free_memory'             => $this->byteFormatter->format($this->memoryStatus['avail_mem']),
+            'fragmentation_percent'   => $this->getFragmentationPercent() . '%',
+            'fragmentation_bytes'     => $this->byteFormatter->format($this->getFragmentationBytes()),
+            'fragmentation_fragments' => $this->getFragmentationSegments(),
+        ];
+    }
+
+    /**
+     * Calculates the percentage of fragmentation
+     *
+     * Note that only blocks < 5MB are considered doe the fragmentation
+     *
+     * @return float The percentage of fragmentation
+     */
+    private function getFragmentationPercent()
+    {
+        $fragmentationSize = 0;
+        $freeTotal         = 0;
+
+        for ($i = 0; $i < $this->memoryStatus['num_seg']; $i++) {
+            foreach ($this->memoryStatus['block_lists'][$i] as $block) {
+                if ($block['size'] < (5 * 1024 * 1024)) {
+                    $fragmentationSize += $block['size'];
+                }
+
+                $freeTotal += $block['size'];
+            }
+        }
+
+        return number_format(($fragmentationSize / $freeTotal) * 100, 2);
+    }
+
+    /**
+     * Calculates the number of bytes of fragmentation
+     *
+     * Note that only blocks < 5MB are considered doe the fragmentation
+     *
+     * @return int The number of bytes of fragmentation
+     */
+    private function getFragmentationBytes()
+    {
+        $fragmentationSize = 0;
+        $freeTotal         = 0;
+
+        for ($i = 0; $i < $this->memoryStatus['num_seg']; $i++) {
+            foreach ($this->memoryStatus['block_lists'][$i] as $block) {
+                if ($block['size'] < (5 * 1024 * 1024)) {
+                    $fragmentationSize += $block['size'];
+                }
+
+                $freeTotal += $block['size'];
+            }
+        }
+
+        return $fragmentationSize;
+    }
+
+    /**
+     * Calculates the number of segments of fragmentation
+     *
+     * Note that only blocks < 5MB are considered doe the fragmentation
+     *
+     * @return int The number of fragmentated segments
+     */
+    private function getFragmentationSegments()
+    {
+        $fragmentationSegments = 0;
+
+        for ($i = 0; $i < $this->memoryStatus['num_seg']; $i++) {
+            $fragmentationSegments += count($this->memoryStatus['block_lists'][$i]);
+        }
+
+        return $fragmentationSegments;
     }
 
     /**
