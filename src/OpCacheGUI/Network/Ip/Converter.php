afe5fe1959de67f20aa46e7771ec2b66e6b379f8
PeeHaa <info@pieterhordijk.com>
2014-10-31 20:41:38 +0100

    Added interface for IP converters

---
+++ src/OpCacheGUI/Network/Ip/Converter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Interface for IP (range) converters
+ *
+ * PHP version 5.5
+ *
+ * @category   OpCacheGUI
+ * @package    Network
+ * @subpackage Ip
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2014 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace OpCacheGUI\Network;
+
+/**
+ * Interface for IP (range) converters
+ *
+ * @category   OpCacheGUI
+ * @package    Network
+ * @subpackage Ip
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Converter
+{
+    /**
+     * Converts an IP address or range into a range to easily check for access
+     *
+     * @param string The IP address / range
+     *
+     * @return int[] Array containing the first and last ip in the range
+     */
+    public function convert($address);
+}
