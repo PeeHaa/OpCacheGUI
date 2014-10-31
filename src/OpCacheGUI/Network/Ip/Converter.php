1aa182407a07fb86726cc53a5a209bfdf24341a8
PeeHaa <info@pieterhordijk.com>
2014-10-31 20:51:47 +0100

    Fixed namespace

--- src/OpCacheGUI/Network/Ip/Converter.php
+++ src/OpCacheGUI/Network/Ip/Converter.php
@@ -12,7 +12,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    1.0.0
  */
-namespace OpCacheGUI\Network;
+namespace OpCacheGUI\Network\Ip;
 
 /**
  * Interface for IP (range) converters
