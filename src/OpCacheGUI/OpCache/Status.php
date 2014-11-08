d035042c28d411d884dc0f50728517b60ae24e6e
PeeHaa <info@pieterhordijk.com>
2014-11-08 15:30:36 +0100

    Refactored globals

--- src/OpCacheGUI/OpCache/Status.php
+++ src/OpCacheGUI/OpCache/Status.php
@@ -14,7 +14,7 @@
 namespace OpCacheGUI\OpCache;
 
 use OpCacheGUI\Format\Byte;
-use OpCacheGUI\I18n\FileTranslator;
+use OpCacheGUI\I18n\Translator;
 
 /**
  * Container for the current status of OpCache
@@ -38,6 +38,11 @@ class Status
     private $byteFormatter;
 
     /**
+     * @var \OpCacheGUI\I18n\Translator A translator
+     */
+    private $translator;
+
+    /**
      * @var array The (unfiltered) output of opcache_get_status()
      */
     private $statusData;
@@ -45,12 +50,14 @@ class Status
     /**
      * Creates instance
      *
-     * @param \OpCacheGUI\Format\Byte $byteFormatter Formatter of byte values
-     * @param array                   $statusData    The (unfiltered) output of opcache_get_status()
+     * @param \OpCacheGUI\Format\Byte     $byteFormatter Formatter of byte values
+     * @param \OpCacheGUI\I18n\Translator $translator    A translator
+     * @param array                       $statusData    The (unfiltered) output of opcache_get_status()
      */
-    public function __construct(Byte $byteFormatter, array $statusData)
+    public function __construct(Byte $byteFormatter, Translator $translator, array $statusData)
     {
         $this->byteFormatter = $byteFormatter;
+        $this->translator    = $translator;
         $this->statusData    = $statusData;
     }
 
@@ -94,23 +101,22 @@ class Status
     public function getGraphMemoryInfo()
     {
         $memory = $this->statusData['memory_usage'];
-        global $translator;
 
         return json_encode([
             [
                 'value' => $memory['used_memory'],
                 'color' => self::RED,
-                'label' => $translator->translate('graph.memory.used'),
+                'label' => $this->translator->translate('graph.memory.used'),
             ],
             [
                 'value' => $memory['free_memory'],
                 'color' => self::GREEN,
-                'label' => $translator->translate('graph.memory.free'),
+                'label' => $this->translator->translate('graph.memory.free'),
             ],
             [
                 'value' => $memory['wasted_memory'],
                 'color' => self::DARK_GREEN,
-                'label' => $translator->translate('graph.memory.wasted'),
+                'label' => $this->translator->translate('graph.memory.wasted'),
             ],
         ]);
     }
@@ -181,23 +187,22 @@ class Status
     public function getGraphKeyStatsInfo()
     {
         $stats = $this->statusData['opcache_statistics'];
-        global $translator;
 
         return json_encode([
             [
                 'value' => $stats['num_cached_scripts'],
                 'color' => self::RED,
-                'label' => $translator->translate('graph.keys.scripts'),
+                'label' => $this->translator->translate('graph.keys.scripts'),
             ],
             [
                 'value' => $stats['max_cached_keys'] - $stats['num_cached_keys'],
                 'color' => self::GREEN,
-                'label' => $translator->translate('graph.keys.free'),
+                'label' => $this->translator->translate('graph.keys.free'),
             ],
             [
                 'value' => $stats['num_cached_keys'] - $stats['num_cached_scripts'],
                 'color' => self::DARK_GREEN,
-                'label' => $translator->translate('graph.keys.wasted'),
+                'label' => $this->translator->translate('graph.keys.wasted'),
             ],
         ]);
     }
@@ -210,23 +215,22 @@ class Status
     public function getGraphHitStatsInfo()
     {
         $stats = $this->statusData['opcache_statistics'];
-        global $translator;
 
         return json_encode([
             [
                 'value' => $stats['hits'],
                 'color' => self::RED,
-                'label' => $translator->translate('graph.hits.hits'),
+                'label' => $this->translator->translate('graph.hits.hits'),
             ],
             [
                 'value' => $stats['misses'],
                 'color' => self::GREEN,
-                'label' => $translator->translate('graph.hits.misses'),
+                'label' => $this->translator->translate('graph.hits.misses'),
             ],
             [
                 'value' => $stats['blacklist_misses'],
                 'color' => self::DARK_GREEN,
-                'label' => $translator->translate('graph.hits.blacklist'),
+                'label' => $this->translator->translate('graph.hits.blacklist'),
             ],
         ]);
     }
