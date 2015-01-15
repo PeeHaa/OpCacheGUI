ba71a584b37bfed99632ea10a51ec933d2f14ae1
PeeHaa <info@pieterhordijk.com>
2015-01-15 20:44:37 +0100

    Only retrieve local ini values

--- src/OpCacheGUI/APCu/Configuration.php
+++ src/OpCacheGUI/APCu/Configuration.php
@@ -47,30 +47,30 @@ class Configuration
         $mmap_file_mask = null;
 
         if (isset($this->configData['apc.mmap_file_mask'])) {
-            $mmap_file_mask = $this->configData['apc.mmap_file_mask']['local_value'];
+            $mmap_file_mask = $this->configData['apc.mmap_file_mask'];
         }
 
         return [
-            'apc.enabled'          => $this->configData['apc.enabled']['local_value'],
-            'apc.enable_cli'       => $this->configData['apc.enable_cli']['local_value'],
-            'apc.writable'         => $this->configData['apc.writable']['local_value'],
-            'apc.preload_path'     => $this->configData['apc.preload_path']['local_value'],
-            'apc.serializer'       => $this->configData['apc.serializer']['local_value'],
-            'apc.entries_hint'     => $this->configData['apc.entries_hint']['local_value'],
-            'apc.ttl'              => $this->configData['apc.ttl']['local_value'],
-            'apc.use_request_time' => $this->configData['apc.use_request_time']['local_value'],
-            'apc.gc_ttl'           => $this->configData['apc.gc_ttl']['local_value'],
-            'apc.smart'            => $this->configData['apc.smart']['local_value'],
-            'apc.slam_defense'     => $this->configData['apc.slam_defense']['local_value'],
-            'apc.shm_segments'     => $this->configData['apc.shm_segments']['local_value'],
-            'apc.shm_size'         => $this->configData['apc.shm_size']['local_value'],
-            'apc.coredump_unmap'   => $this->configData['apc.coredump_unmap']['local_value'],
+            'apc.enabled'          => $this->configData['apc.enabled'],
+            'apc.enable_cli'       => $this->configData['apc.enable_cli'],
+            'apc.writable'         => $this->configData['apc.writable'],
+            'apc.preload_path'     => $this->configData['apc.preload_path'],
+            'apc.serializer'       => $this->configData['apc.serializer'],
+            'apc.entries_hint'     => $this->configData['apc.entries_hint'],
+            'apc.ttl'              => $this->configData['apc.ttl'],
+            'apc.use_request_time' => $this->configData['apc.use_request_time'],
+            'apc.gc_ttl'           => $this->configData['apc.gc_ttl'],
+            'apc.smart'            => $this->configData['apc.smart'],
+            'apc.slam_defense'     => $this->configData['apc.slam_defense'],
+            'apc.shm_segments'     => $this->configData['apc.shm_segments'],
+            'apc.shm_size'         => $this->configData['apc.shm_size'],
+            'apc.coredump_unmap'   => $this->configData['apc.coredump_unmap'],
             'apc.mmap_file_mask'   => $mmap_file_mask,
-            'apc.rfc1867'          => $this->configData['apc.rfc1867']['local_value'],
-            'apc.rfc1867_freq'     => $this->configData['apc.rfc1867_freq']['local_value'],
-            'apc.rfc1867_name'     => $this->configData['apc.rfc1867_name']['local_value'],
-            'apc.rfc1867_prefix'   => $this->configData['apc.rfc1867_prefix']['local_value'],
-            'apc.rfc1867_ttl'      => $this->configData['apc.rfc1867_ttl']['local_value'],
+            'apc.rfc1867'          => $this->configData['apc.rfc1867'],
+            'apc.rfc1867_freq'     => $this->configData['apc.rfc1867_freq'],
+            'apc.rfc1867_name'     => $this->configData['apc.rfc1867_name'],
+            'apc.rfc1867_prefix'   => $this->configData['apc.rfc1867_prefix'],
+            'apc.rfc1867_ttl'      => $this->configData['apc.rfc1867_ttl'],
         ];
     }
 }
