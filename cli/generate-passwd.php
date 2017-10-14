7631cba5d0ace07be4747dec8e19f65fcfc201c7
Pieter Hordijk <info@pieterhordijk.com>
2017-10-15 01:39:34 +0200

    Added password generation script

---
+++ cli/generate-passwd.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 'WARNING: Password will be visible on the command line and will be stored in your history.' . PHP_EOL;
+echo 'Don\'t use this script if other people have access to your shell (history)!' . PHP_EOL . PHP_EOL;
+
+echo 'Enter your password: ';
+
+$password = trim(fgets(STDIN));
+
+if (empty($password)) {
+    echo 'ERROR: Password empty...';
+    exit;
+}
+
+echo password_hash($argv[0], PASSWORD_DEFAULT, ['cost' => 12]) . PHP_EOL;
